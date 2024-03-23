8ab9d33fbcef3a16212ba3d309c8d322c3210094
Ankit <ankitkumar7277410093@gmail.com>
2024-03-23 16:22:35 +0530

    test

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,2 +1,11 @@
 //2. Write a C program to add two numbers.
 
+#include<stdio.h>
+int main()
+{
+    int a = 5;
+    int b = 3;
+    int c = a + b;
+    printf("The Addition of two number is: %d" , c);
+    return 0;
+}
